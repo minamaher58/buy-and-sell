191cf5ad16fd193dc850f8c9083568e7d9da0962
minamaher58 <minamahertalaat58@gmail.com>
2020-11-05 14:09:24 +0200

    front end app in angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListingsPageComponent,
+    ListingsDetailPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent,
+    ListingDataFormComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/listings-page/listings-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ListingsService } from '../listings.service';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-listings-page',
+  templateUrl: './listings-page.component.html',
+  styleUrls: ['./listings-page.component.css']
+})
+export class ListingsPageComponent implements OnInit {
+
+  listings: Listing[] = [];
+
+  constructor(private listingsService: ListingsService) { }
+
+  ngOnInit(): void {
+    this.listings = this.listingsService.getListings();
+  }
+
+}
